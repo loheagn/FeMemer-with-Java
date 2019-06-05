085b443577ecb979694d3d9ad0818b14ff0d33e0
loheagn <linan_123@buaa.edu.cn>
2019-06-05 19:27:27 +0800

    完成微信爬取

--- fememer-server/src/main/java/com/loheagn/fememer/ChromeHeadless.java
+++ fememer-server/src/main/java/com/loheagn/fememer/ChromeHeadless.java
@@ -1,17 +1,29 @@
 package com.loheagn.fememer;
 
 import java.io.*;
+import java.net.*;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.*;
 
 public class ChromeHeadless {
-    private String chromeDriverPath = "/usr/local/bin/chromedriver";
-    private String chromePath = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
-    WebDriver driver;
+    private String chromeDriverPath;
+    private String chromePath;
+    private WebDriver driver;
 
     public ChromeHeadless() {
+        setChromeDriverPath("/usr/local/bin/chromedriver");
+        setChromePath("/Applications/Google Chrome.app/Contents/MacOS/Google Chrome");
         System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200", "--ignore-certificate-errors");
+        options.setBinary(chromePath);
+        driver = new ChromeDriver(options);
+    }
+
+    public ChromeHeadless(String chromeDriverPath, String chromePath) {
+        this.chromeDriverPath = chromeDriverPath;
+        this.chromePath = chromePath;
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200", "--ignore-certificate-errors");
         options.setBinary(chromePath);
@@ -23,7 +35,36 @@ public class ChromeHeadless {
             driver.get(url);
             return driver.getPageSource();
         } catch (Exception e) {
-            return "抓取网页错误！";
+            return "抓取网页源代码错误！";
+        }
+    }
+
+    /**
+     * 下载图片到本地，保存到指定位置
+     * 
+     * @param urlString 图片的url
+     * @param fileName  本地存放文件的位置，这是个全路径，包含路径和文件名
+     * @return 返回图片文件对象
+     */
+    public File downImageFile(String urlString, String fileName) {
+        try {
+            URL url = new URL(urlString);
+            URLConnection urlConnection = url.openConnection();
+            urlConnection.setConnectTimeout(5000);
+            InputStream inputStream = urlConnection.getInputStream();
+            File imageFile = new File(fileName);
+            OutputStream outputStream = new FileOutputStream(imageFile);
+            byte[] bytes = new byte[4096];
+            int length;
+            while ((length = inputStream.read(bytes)) > 0) {
+                outputStream.write(bytes, 0, length);
+            }
+            outputStream.close();
+            inputStream.close();
+            return imageFile;
+        } catch (Exception e) {
+            System.err.println("下载图片" + fileName + "失败");
+            return null;
         }
     }
 
@@ -55,12 +96,30 @@ public class ChromeHeadless {
         this.chromePath = chromePath;
     }
 
+    /**
+     * @return the driver
+     */
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    /**
+     * @param driver the driver to set
+     */
+    public void setDriver(WebDriver driver) {
+        this.driver = driver;
+    }
+
     public static void main(String[] args) throws IOException {
         ChromeHeadless chromeHeadless = new ChromeHeadless();
-        File file = new File("HtmlDemo.html");
-        OutputStream outputStream = new FileOutputStream(file);
-        outputStream.write(
-                chromeHeadless.getPageSource("https://www.zhihu.com/question/327780925/answer/704644555").getBytes());
-        System.out.println(chromeHeadless.getPageSource("https://www.zhihu.com/question/327780925/answer/704644555"));
+        // File file = new File("ttt.html");
+        // OutputStream outputStream = new FileOutputStream(file);
+        // outputStream.write(chromeHeadless.getPageSource(
+        // "https://mmbiz.qpic.cn/mmbiz_jpg/b96CibCt70iaaicHUKVLMp2vK1qtPdpGSbd4QAW2ItYkgsPJ1KPE8PPuEYzRVCnlcencLAhbFlFdnEhmVTNw0KSRQ/640?tp=webp&wxfrom=5&wx_lazy=1&wx_co=1")
+        // .getBytes());
+        // outputStream.close();
+        chromeHeadless.downImageFile(
+                "https://mmbiz.qpic.cn/mmbiz_jpg/b96CibCt70iaaicHUKVLMp2vK1qtPdpGSbd4QAW2ItYkgsPJ1KPE8PPuEYzRVCnlcencLAhbFlFdnEhmVTNw0KSRQ/640?tp=webp&wxfrom=5&wx_lazy=1&wx_co=1",
+                "test.png");
     }
 }
---
+++ fememer-server/src/main/java/com/loheagn/fememer/Render.java
@@ -0,0 +1,44 @@
+package com.loheagn.fememer;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+import freemarker.template.*;
+
+/**
+ * Render
+ */
+public class Render {
+
+    public static String render(Map<String, String> objects) {
+        try {
+            // 初始化
+            Configuration configuration = new Configuration(Configuration.VERSION_2_3_28);
+            configuration.setDirectoryForTemplateLoading(new File("templates"));
+            configuration.setDefaultEncoding("UTF-8");
+            configuration.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+
+            Template template = configuration.getTemplate("main.html");
+            Writer writer = new StringWriter();
+            template.process(objects, writer);
+            return writer.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static String renderArticle(String title, String author, String bigSource, String tag, String webUrl,
+            String source, String content) {
+        Map<String, String> objects = new HashMap<String, String>();
+        objects.put("title", title);
+        objects.put("author", author);
+        objects.put("bigSource", bigSource);
+        objects.put("source", source);
+        objects.put("tag", tag);
+        objects.put("webUrl", webUrl);
+        objects.put("content", content);
+        return render(objects);
+    }
+}
